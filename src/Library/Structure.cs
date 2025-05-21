248ac72da1b16d4edb8bb05295cee918d36ac876
Nicolas Vazquez <Nicolas Vazquez>
2025-05-21 19:42:18 -0300

    StructureInterface

---
+++ src/Library/Structure.cs
@@ -0,0 +1,10 @@
+﻿namespace DefaultNamespace;
+
+public interface IStructure
+{
+    string Name { get; set; }
+    int Capacity { get; set; }
+    int location { get; set; }
+    int Cost { get; set; }
+    int time_of_construction { get; set; }
+}
