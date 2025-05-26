09e591f72785a13ebaaa3c42c261ecc876a330cc
Santiago <st281306@fi365.ort.edu.uy>
2025-05-26 19:21:51 -0300

    building.cs first functionalities

--- src/Library/Building.cs
+++ src/Library/Building.cs
@@ -2,5 +2,14 @@ namespace Library;
 
 public class Building
 {
+    public BuildingType Type { get; set; }
+    public Player Owner { get; set; }
+    public int Capacity { get; set; }
     
+    public Building(BuildingType type, Player owner, int capacity)
+    {
+        Type = type;
+        Owner = owner;
+        Capacity = capacity;
+    }
 }
