8fb1bca45fe523f6c01539676a372330581aebed
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-05-26 18:36:12 -0300

    Clase player

--- src/Library/Player.cs
+++ src/Library/Player.cs
@@ -2,5 +2,31 @@ namespace Library;
 
 public class Player
 {
-    
+    public string Name { get; set; }
+    public int Food { get; set; }
+    public int Wood { get; set; }
+    public int Gold { get; set; }
+
+    public Player(string name)
+    {
+        Name = name;
+        Food = 0;
+        Wood = 0;
+        Gold = 0;
+    }
+
+    public void CollectFood(int amount)
+    {
+        Food += amount;
+    }
+
+    public void CollectWood(int amount)
+    {
+        Wood += amount;
+    }
+
+    public void CollectGold(int amount)
+    {
+        Gold += amount;
+    }
 }
