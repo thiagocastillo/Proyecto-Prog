2f0f62646dbebe62c9c3ff728c74c776d4fe5b35
Santiago <st281306@fi365.ort.edu.uy>
2025-05-26 19:15:40 -0300

    civilization.cs 1st functionalities

--- src/Library/Civilization.cs
+++ src/Library/Civilization.cs
@@ -2,5 +2,14 @@ namespace Library;
 
 public class Civilization
 {
+    public string Name { get; set; }
+    public List<string> Bonifications { get; set; }
+    public List<string> SpecialUnits { get; set; }
     
+    public Civilization(string name, List<string> bonifications, List<string> specialUnits)
+    {
+        Name = name;
+        Bonifications = bonifications;
+        SpecialUnits = specialUnits;
+    }
 }
