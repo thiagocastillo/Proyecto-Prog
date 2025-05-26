a7977cf1b098f4c55929482309f1c5d898e34ded
Santiago <st281306@fi365.ort.edu.uy>
2025-05-26 19:26:20 -0300

    game.cs first functionalities

--- src/Library/Game.cs
+++ src/Library/Game.cs
@@ -2,5 +2,18 @@ namespace Library;
 
 public class Game
 {
+    public Map Map { get; set; }
+    public List<Player> Players { get; set; }
+    public int CurrentPlayerIndex { get; set; }
     
+    public Game(Map map, List<Player> players)
+    {
+        Map = map;
+        Players = players;
+        CurrentPlayerIndex = 0;
+    }
+
+    public void StartGame();
+    public void PlayTurn();
+    public bool IsGameOver();
 }
