aad0fe91f9c14d870b072cc53eca96c6d63cdb53
thiagocastillo <Thiago.castillo.cbt@gmail.com>
2025-05-26 20:02:40 -0300

    cambios a point

--- src/Library/Point.cs
+++ src/Library/Point.cs
@@ -1,7 +1,15 @@
 namespace Library;
 
-public struct Point
-{
-    public int X { get; set; }
-    public int Y { get; set; }
-}
+   public class Point
+   {
+       public int X { get; set; }
+       public int Y { get; set; }
+
+       public Point() { } // Constructor sin parámetros (opcional, pero común en clases)
+
+       public Point(int x, int y)
+       {
+           X = x;
+           Y = y;
+       }
+   }
