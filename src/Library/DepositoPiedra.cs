77b502e33cdca39986fa41a11506e7d1cd680f45
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-05-28 14:00:55 -0300

    Correccion IAlmacenamiento

--- src/Library/DepositoPiedra.cs
+++ src/Library/DepositoPiedra.cs
@@ -10,4 +10,11 @@ public class DepositoPiedra : IAlmacenamiento
     {
         Propietario = propietario;
     }
+    
+    public double Eficiencia(int distancia)
+    {
+        if (distancia <= 1) return 1.0;
+        if (distancia >= 10) return 0.1;
+        return 1.0 - (distancia * 0.1);
+    }
 }
