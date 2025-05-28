b37c809744854d93b9900578f1a68197bf4565cd
Nicolas Vazquez <Nicolas Vazquez>
2025-05-28 15:45:41 -0300

    Cambios en clase casa

---
+++ src/Library/Casa2.cs
@@ -0,0 +1,30 @@
+﻿namespace Library;
+
+public class Casa2 : IEdificio
+{ 
+    public Jugador Propietario { get; private set; }
+    public Point Posicion { get; set; }
+    public const int AumentoPoblacion = 5;
+
+    private int max_aldeanos = 20;
+    private int max_unidadMilitar = 30;
+    
+    public int CantidadAldeanos { get; set; }
+    public int CantidadUnidadesMilitar { get; set; }
+    
+    public Casa2(Jugador propietario)
+    {
+        Propietario = propietario;
+        propietario.AumentarPoblacionMaxima(AumentoPoblacion);
+    }
+
+    public bool CantidadAldeano()
+    {
+        return CantidadAldeanos <= max_aldeanos;
+    }
+
+    public bool CantidadUnidadMilitar()
+    {
+        return CantidadUnidadesMilitar <= max_unidadMilitar;
+    }
+}
