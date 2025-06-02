021426ff674c0e92cbcb7f7792811fdbab1215ed
Nicolas Vazquez <Nicolas Vazquez>
2025-06-02 00:37:33 -0300

    Tiempo de cons de cuartel

--- src/Library/Cuartel.cs
+++ src/Library/Cuartel.cs
@@ -5,6 +5,12 @@ public class Cuartel : IEdificio
     public Jugador Propietario { get; private set; }
     public Point Posicion { get; set; }
     public int Vida { get; set; }
+    
+    private TiempoConstruccion tiempoconstruccion;
+
+    public int TiempoConstruccionTotal => tiempoconstruccion.TiempoTotal;
+    public int TiempoConstruccionRestante => tiempoconstruccion.TiempoRestante;
+    public bool EstaConstruido => tiempoconstruccion.EstaCompleta;
 
     public Cuartel(Jugador propietario)
     {
