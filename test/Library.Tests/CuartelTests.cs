46cb87f197d0dada249fa059c480a56f1b45ac49
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-06-17 15:29:26 -0300

    CuartelTests

---
+++ test/Library.Tests/CuartelTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Library.Tests;
+
+public class CuartelTests
+{
+    private Jugador jugador;
+
+    [SetUp]
+    public void Setup()
+    {
+        var civilizacion = new Civilizacion("Aztecas", new List<string>(), "GuerreroJaguar");
+        jugador = new Jugador("Luis", civilizacion);
+    }
+
+    [Test]
+    public void Constructor_AsignacionCorrecta()
+    {
+        var cuartel = new Cuartel(jugador);
+        
+        Assert.AreEqual(jugador, cuartel.Propietario);
+        Assert.AreEqual(10000, cuartel.Vida);
+    }
+
+    [Test]
+    public void TiempoConstruccion_Default_NoConstruido()
+    {
+        var cuartel = new Cuartel(jugador);
+        
+        Assert.IsFalse(cuartel.EstaConstruido);
+    }
+}
