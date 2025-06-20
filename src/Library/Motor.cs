13354efaa6f61b98cd17bb5d558cce22b93c225d
Santiago <st281306@fi365.ort.edu.uy>
2025-06-20 18:47:12 -0300

    mini fix in motor

--- src/Library/Motor.cs
+++ src/Library/Motor.cs
@@ -67,7 +67,7 @@ public class Motor
                     
                     return _fachada.AtacarUnidad(argumentos[0], int.Parse(argumentos[1]), int.Parse(argumentos[2]));
                 
-                case "atacarudificio":               //ver...
+                case "atacaredificio":               //ver...
                     
                     if (argumentos.Count < 3)
                         return "Faltan argumentos en comando, recordar sintaxis: atacaredificio <nombre> <idAtacante> <idObjetivo>";
