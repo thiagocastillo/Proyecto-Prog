aa46fbd6d9842c1f33ccb1f50a32e23e7c581f59
Nicolas Vazquez <Nicolas Vazquez>
2025-06-22 23:32:36 -0300

    clase Madera

---
+++ src/Library/Madera.cs
@@ -0,0 +1,10 @@
+﻿namespace Library;
+
+public class Madera : RecursoNatural
+{
+
+    public Madera(int vidaBase, Point ubicacion)
+        : base("Madera", vidaBase, 0.40, ubicacion)
+    {
+    }
+}
