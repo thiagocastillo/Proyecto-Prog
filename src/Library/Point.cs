fdd509a9e5aafed6c7049ffb187001c45942f9ab
Nicolas Vazquez <Nicolas Vazquez>
2025-06-23 19:42:10 -0300

    Comentarios en Point

--- src/Library/Point.cs
+++ src/Library/Point.cs
@@ -2,12 +2,12 @@ namespace Library;
 
    public class Point
    {
-       public int X { get; set; }
-       public int Y { get; set; }
+       public int X { get; set; }  //Coordenadea del eje de las abscisas
+       public int Y { get; set; }  //Coordenada del eje de las ordenadas
 
-       public Point() { } 
+       public Point() { }  //Constructor vacio, para iniciar en (0,0)
 
-       public Point(int x, int y)
+       public Point(int x, int y)  //Constructor que recibe parametro, las coordenadas
        {
            X = x;
            Y = y;
