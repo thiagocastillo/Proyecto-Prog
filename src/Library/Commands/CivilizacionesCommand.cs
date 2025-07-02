c33e418012cc98680e5199de5d40a7be7b3adf3e
thiagocastillo <thiago.castillo.cbt@gmail.com>
2025-07-02 20:17:58 +0200

    civilizacionescommand

---
+++ src/Library/Commands/CivilizacionesCommand.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using Library.Domain;
+using System.Collections.Generic;
+
+public class CivilizacionesCommand : ModuleBase<SocketCommandContext>
+{
+    private readonly JuegoFachada _fachada = new JuegoFachada();
+
+    [Command("civilizaciones")]
+    [Summary("Lista las civilizaciones disponibles.")]
+    public async Task ExecuteAsync()
+    {
+        List<string> civs = _fachada.ObtenerCivilizacionesDisponibles();
+        await ReplyAsync("Civilizaciones disponibles:\n" + string.Join("\n", civs));
+    }
+}
