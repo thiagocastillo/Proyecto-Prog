9b27ed5ae0afdea3c9729822919553fca0dc9884
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-07-03 16:22:01 -0300

    Comando MoverUnidades

---
+++ src/Library/Commands/MoverUnidadesCommand.cs
@@ -0,0 +1,28 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using Library.Domain;
+
+namespace Ucu.Poo.DiscordBot.Commands;
+
+public class MoverUnidadesCommand : ModuleBase<SocketCommandContext>
+{
+    private readonly JuegoFachada _fachada = new JuegoFachada();
+
+    [Command("moverunidades")]
+    [Summary("Mueve multiples unidades a una posicion. Sintaxis: moverunidades <nombreJugador> <x> <y> <id1> <id2>")]
+    public async Task ExecuteAsync(string nombreJugador, int x, int y, [Remainder] string idsTexto)
+    {
+        
+        string[] partes = idsTexto.Split(' ');
+        List<int> ids = new List<int>();
+        foreach (string parte in partes)
+        {
+            if (!string.IsNullOrWhiteSpace(parte))
+            {
+                ids.Add(int.Parse(parte));
+            }
+        }
+        var resultados = _fachada.MoverUnidades(nombreJugador, ids, new Point(x, y));
+        await ReplyAsync(string.Join("\n", resultados));
+        }
+    }
