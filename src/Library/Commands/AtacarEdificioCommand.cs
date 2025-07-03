f9c045d0299e1202e9e3e1de34d51a70b4c9073e
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-07-03 16:28:30 -0300

    Comando AtacarEdificio

---
+++ src/Library/Commands/AtacarEdificioCommand.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using Library.Domain;
+
+namespace Ucu.Poo.DiscordBot.Commands;
+
+public class AtacarEdificioCommand : ModuleBase<SocketCommandContext>
+{
+    private readonly JuegoFachada _fachada = new JuegoFachada();
+
+    [Command("atacaredificio")]
+    [Summary("Ordena a una unidad militar atacar un edificio enemigo. Sintaxis: atacaredificio <nombreJugadorAtacante> <idAtacante> <nombreJugadorObjetivo> <idObjetivo>")]
+    public async Task ExecuteAsync(string nombreJugadorAtacante, int idAtacante, string nombreJugadorObjetivo, int idObjetivo)
+    {
+        string resultado = _fachada.AtacarEdificio(nombreJugadorAtacante, idAtacante, nombreJugadorObjetivo, idObjetivo);
+        await ReplyAsync(resultado);
+    }
+}
