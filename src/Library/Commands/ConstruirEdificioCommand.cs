1cf1cca05e643fc576cb951cdaf832d2ccedc2bc
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-07-05 11:30:14 -0300

    Comando COnstruiEdificio

---
+++ src/Library/Commands/ConstruirEdificioCommand.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using Library.Domain;
+
+public class ConstruirEdificioCommand : ModuleBase<SocketCommandContext>
+{
+    private readonly JuegoFachada _fachada = new JuegoFachada();
+
+    [Command("construiredificio")]
+    [Summary("Construye un edificio en la posicion indicada. Sintaxis: construiredificio <nombreJugador> <tipoEdificio> <x> <y>")]
+    public async Task ExecuteAsync(string nombreJugador, string tipoEdificio, int x, int y)
+    {
+        _fachada.ConstruirEdificio(nombreJugador, tipoEdificio, new Point(x, y));
+        await ReplyAsync($"Edificio '{tipoEdificio}' contruido en ({x}, {y}) para el jugador '{nombreJugador}'");
+    }
+}
